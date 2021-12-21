23c926eec5028c20f49d8669e2e29ed690d54a3f
Matheus Fellipe <matheus060399@gmail.com>
2021-12-21 14:55:13 -0400

    sendEmail

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
-import * as MailCompose from 'expo-mail-composer';
+import * as MailComposer from 'expo-mail-composer';
 
 import LogoImg from '../../assets/logo.png';
 
@@ -10,13 +10,18 @@ import styles from './styles';
 
 export default function Detail() {
     const navigation = useNavigation();
+    const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$120,00';
 
     function navigateBack() {
         navigation.goBack()
     }
 
     function sendMail() {
-
+        MailComposer.composeAsync({
+            subject: 'Herói do caso: Cadelinha atropelada',
+            recipients: ['matheus@gmail.com'],
+            body: message,
+        })
     }
 
     function sendWhatsapp() {
@@ -61,7 +66,7 @@ export default function Detail() {
                         <Text style={styles.actionText}>Whatsapp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.action} onPress={() => { }}>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
                         <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>
